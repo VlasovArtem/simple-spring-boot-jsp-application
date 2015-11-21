042726c0144bc38a70646a281dd3b04de64fe859
Vlasov Artem <vlasovartem21@gmail.com>
2015-11-22 01:58:34 +0200

    Update ServletContextConfig resolve issue with 404

--- src/main/java/com/vlasovartem/tvspace/config/ServletContextConfig.java
+++ src/main/java/com/vlasovartem/tvspace/config/ServletContextConfig.java
@@ -19,7 +19,7 @@ public class ServletContextConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**")
-                .addResourceLocations("/resources");
+                .addResourceLocations("/resources/");
     }
 
     @Bean
